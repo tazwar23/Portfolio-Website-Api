5d124d48d4f9eb48acdaf985902f1650f047e1d2
tazwar23 <atazwar@myseneca.ca>
2023-02-27 03:25:12 -0500

    Mongo connection established

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
-console.log("hello world")
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const app = express();
+var HTTP_PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGODB_CONN_STRING;
+
+console.log(uri);
+
+mongoose.connect(uri,{ useNewUrlParser: true});
+
+const connection = mongoose.connection;
+
+connection.once('open', () =>{
+    console.log("MongoDB database connection established successfully");
+})
+
+// const db = mongoose.connect(uri);
+// db.once('open', ()=>{console.log("YAAAAAAAAAAAAAAAAAAAA")})
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+
+// setup http server to listen on HTTP_PORT
+//app.listen(HTTP_PORT, onHttpStart);
+ app.listen(HTTP_PORT, ()=>{
+       console.log(`server listening on: ${HTTP_PORT}`);
+});
