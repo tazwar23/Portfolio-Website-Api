75699eab5c921d8e35a49d3cce92a4591e9f9052
tazwar23 <atazwar@myseneca.ca>
2023-03-01 04:56:49 -0500

    Routes declared

---
+++ routes/projects.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+let Project = require('../models/project.model');
+
+router.route('/').get((req,res) => {
+
+})
+
+router.route('/:id').get((req,res) =>{
+
+})
+
+router.route('/').post((req,res) => {
+
+})
+
+router.route('/:id').put((req,res) => {
+
+})
+
+router.route('/:id').delete((req,res) => {
+    
+})
+
+module.exports = router;
