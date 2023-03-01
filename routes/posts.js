9ea1b49956ef12845f2a38cda8f9ec2fa84ccdc7
tazwar23 <atazwar@myseneca.ca>
2023-03-01 04:57:27 -0500

    minor clean up

---
+++ routes/posts.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+let Post = require('../models/posts.model');
+
+router.route('/').get((req,res) => {
+
+})
+
+router.route('/:id').get((req,res) =>{
+
+})
+
+router.route('/').post((req,res) => {
+
+})
+
+router.route('/:id').put((req,res) => {
+
+})
+
+router.route('/:id').delete((req,res) => {
+    
+})
+
+module.exports = router;
